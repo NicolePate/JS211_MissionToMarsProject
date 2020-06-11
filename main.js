53e60899b442c62b89f2fab4664fd320e33e3683
NicolePate <nicole.pate512@gmail.com>
2020-06-11 15:41:03 -0500

    all passing

--- main.js
+++ main.js
@@ -11,8 +11,36 @@ const jobTypes = {
 
 // Your code will go here
 
+class CrewMember {
+  constructor(name, job, specialSkill, ship){
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+  }
+  enterShip = (ship) => {
+    this.ship = ship;
+    this.ship.crew.push(this)
+  }
+};
 
 
+class Ship {
+  constructor(name, type, ability, crew){
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = []
+  }
+  missionStatement = () => {
+    if (this.crew.length === 0) {
+      return "Can't perform a mission yet."
+    } else if (this.crew.length !== 0 && this.name === "Mars Ascent Vehicle") {
+      return "Ascend into low orbit"
+    } else if (this.crew.length !== 0 && this.name === "Hermes")
+      return "Interplanetary Space Travel";
+  }
+};
 
 
 
